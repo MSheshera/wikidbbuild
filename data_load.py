af01262ba424a756e9c0acd764d3945624062b19
MSheshera <mssheshera@yahoo.com>
2016-11-21 12:17:27 -0500

    Add code that created the binary task dataset

--- data_load.py
+++ data_load.py
@@ -14,12 +14,18 @@ CONTENT_SUFFIX = u"_content"
 TEMPLATE_SUFFIX = u"_template"
 CATEGORY_SUFFIX = u"_category"
 
-def isWanted(title, categories):
+def isWanted(page_obj, multi):
     """
     Looks at the prefix and categories of the page title and says true 
     if it is a normal article page.
     """
-    # Non-article pages have a prefix ending in ':' in their title
+    # If its for multi then you want to say false if there are no templates
+    # in use.
+    if multi is True:
+    	if len(page_obj.templates) == 0:
+    		return False
+
+    # Non-article pages have a prefix ending in ':' in their title		
     unwanted_re = u'[a-z\s]+\:.+'
     dis_re = u'.*disambiguation.*'
     obs_re = u'.*Complete list of encyclopedia topics \(obsolete\)'
@@ -27,12 +33,12 @@ def isWanted(title, categories):
     want = True
     
     # Check for disambiguation pages
-    for cat in categories:
+    for cat in page_obj.categories:
         if re.match(pattern=dis_re, string=cat, flags=re.IGNORECASE) or re.match(pattern=obs_re, string=cat, flags=re.IGNORECASE):
             want = False        
     
     # Check for other unwanted pages with prefixes in their names.            
-    if re.match(pattern=unwanted_re, string=title, flags=re.IGNORECASE):
+    if re.match(pattern=unwanted_re, string=page_obj.title, flags=re.IGNORECASE):
         want = False
     
     return want
@@ -77,10 +83,10 @@ def populate_tables(article_dict):
     cursor = cnx.cursor()
     
     add_article = ("INSERT INTO article_tb "
-        "(page_id, page_title) "
-        "VALUES (%s, %s)")
+        "(page_id, page_title, needs) "
+        "VALUES (%s, %s, %s)")
     for key, val in article_dict.iteritems():
-        article_data = (key, val[0])
+        article_data = [key, val[0], val[1]]
         cursor.execute(add_article, article_data)
     cnx.commit()
   
@@ -88,7 +94,7 @@ def populate_tables(article_dict):
         "(page_id, content_path, template_path, category_path) "
         "VALUES (%s, %s, %s, %s)")
     for key, val in article_dict.iteritems():
-        article_path_data = (key, val[1], val[2], val[3])
+        article_path_data = [key, val[3], val[2], val[4]]
         cursor.execute(add_article_path, article_path_data)
     cnx.commit()
     cursor.close()
@@ -101,35 +107,45 @@ def save_all_article_data(page_obj, rand_pid):
     Write all article related content to disk. The content, the categories,
     and the templates. Writing all content to file as pickle dumps so that
     they can be read back in just like that :) 
+    Returns:
+    	paths_pp:[title,if_template,template_path,content_path,category_path]
     """
     
-    paths = []
-    paths.append(unicode(page_obj.title, 'utf-8'))
+    paths_pp = []
+    paths_pp.append(unicode(page_obj.title, 'utf-8'))
+
+    # Already filtered for infoboxes at this point.
+    infobox_template = page_obj.templates
+    f_path = TEMPLATE_PATH + u"/" + rand_pid + TEMPLATE_SUFFIX + u".pd"
+    with open(f_path,'w') as itf:
+        pickle.dump(infobox_template, itf)
+    # Annotation help; If the article has a template then needs=1.
+    if len(infobox_template) == 0: 
+    	paths_pp.append(0)
+    else: 
+    	paths_pp.append(1)
+    
+    paths_pp.append(f_path)
 
     content = page_obj.getWikiText()
     f_path = CONTENT_PATH + u"/" + rand_pid + CONTENT_SUFFIX + u".pd"
     with open(f_path,'w') as cf:
         pickle.dump(unicode(content,'utf-8'), cf)
-    paths.append(f_path)
-
-    infobox_template = getInfoboxes(page_obj.getTemplates())
-    f_path = TEMPLATE_PATH + u"/" + rand_pid + TEMPLATE_SUFFIX + u".pd"
-    with open(f_path,'w') as itf:
-        pickle.dump(infobox_template, itf)
-    paths.append(f_path)
+    paths_pp.append(f_path)
 
     category = page_obj.getCategories()
     f_path = CATEGORY_PATH + u"/" + rand_pid + CATEGORY_SUFFIX + u".pd"
     with open(f_path,'w') as caf:
         pickle.dump(category, caf)
-    paths.append(f_path)
+    paths_pp.append(f_path)
+    return paths_pp
 
-    return paths
 
-
-def fill_article_table(num_art_want):
+def fill_article_table(num_art_want, multi):
     """
-    Getting the data on my own.
+    Write content to file and save paths etc to the database.
+    When multi is true we're building the dataset for the multi-class
+    classification task. Binary task otherwise.
     """
     
     site = wiki.Wiki(u"https://en.wikipedia.org/w/api.php")
@@ -146,14 +162,18 @@ def fill_article_table(num_art_want):
         if rand_pid not in art_dict:
             try:
                 p = page.Page(site=site, namespace=NS, pageid=rand_pid)
-            
+
                 # Ensuring that its not a page from a different namespace.
                 # This shouldn't happen according to what the API 
                 # documentation says. IDK why its happening.
                 p.getCategories()
-                if isWanted(title=p.title, categories=p.categories):
+                # Over writing the page objects' list of templates with just 
+                # the infobox templates; seems like a bad thing to do but idk.
+                p.templates = getInfoboxes(p.getTemplates())
+
+                if isWanted(p, multi=multi):
                     count += 1
-                    print p.title
+                    print "dl:", p.title
                     rand_pid = unicode(str(rand_pid),'utf-8')
                     data = save_all_article_data(p, rand_pid)
 
@@ -164,8 +184,4 @@ def fill_article_table(num_art_want):
     populate_tables(art_dict)
 
 if __name__ == '__main__':
-    templates = [u'Template:1x', u'Template:;', u'Template:Ambox', u'Template:Anglicise rank', u'Template:COLON', u'Template:Category handler', u'Template:Citation needed', u'Template:Cite book', u'Template:Cite journal', u'Template:Colon', u'Template:Column-width', u'Template:Commons', u'Template:Commons category', u'Template:Delink', u'Template:Expand section', u'Template:Fix', u'Template:Fix/category', u'Template:Italic title', u'Template:Lpsn', u'Template:Main', u'Template:Main article', u'Template:PAGENAMEBASE', u'Template:Reflist', u'Template:Side box', u'Template:Sister project', u'Template:Str find', u'Template:Str left', u'Template:Taxobox', u'Template:Taxobox/Error colour', u'Template:Taxobox/core', u'Template:Taxobox colour', u'Template:Taxobox name', u'Template:Taxonomy', u'Template:Taxonomy/nobreak', u'Module:Arguments', u'Module:Category handler', u'Module:Category handler/blacklist', u'Module:Category handler/config', u'Module:Category handler/data', u'Module:Category handler/shared', u'Module:Citation/CS1', u'Module:Citation/CS1/COinS', u'Module:Citation/CS1/Configuration', u'Module:Citation/CS1/Date validation', u'Module:Citation/CS1/Identifiers', u'Module:Citation/CS1/Utilities', u'Module:Citation/CS1/Whitelist', u'Module:Delink', u'Module:Hatnote', u'Module:Hatnote list', u'Module:InfoboxImage', u'Module:Italic title', u'Module:Main', u'Module:Message box', u'Module:Message box/configuration', u'Module:Namespace detect/config', u'Module:Namespace detect/data', u'Module:No globals', u'Module:Side box', u'Module:String', u'Module:Unsubst', u'Module:Yesno']
-    templates1 = [u'Template:Category handler', u'Template:Cite web', u'Template:Column-count', u'Template:Commons', u'Template:Commons category', u'Template:Convert', u'Template:DMCA', u'Template:Dated maintenance category', u'Template:Dead link', u'Template:FULLROOTPAGENAME', u'Template:Fix', u'Template:Fix/category', u'Template:HMS', u'Template:Ifsubst', u'Template:Infobox ship begin', u'Template:Infobox ship career', u'Template:Infobox ship characteristics', u'Template:Infobox ship image', u'Template:Main other', u'Template:Military navigation', u'Template:NVR', u'Template:Navbox', u'Template:Ns has subpages', u'Template:Other ships', u'Template:Reflist', u'Template:Sclass-', u'Template:Sclass/core', u'Template:Ship', u'Template:Ship in active service', u'Template:Side box', u'Template:Sister project', u'Template:Ticonderoga class cruiser', u'Template:Ticonderoga class cruiser armament mark 41', u'Template:Ticonderoga class cruiser beam', u'Template:Ticonderoga class cruiser complement', u'Template:Ticonderoga class cruiser displacement', u'Template:Ticonderoga class cruiser draft', u'Template:Ticonderoga class cruiser draught', u'Template:Ticonderoga class cruiser length', u'Template:Ticonderoga class cruiser propulsion', u'Template:Ticonderoga class cruiser sensors', u'Template:Ticonderoga class cruiser speed', u'Template:USN flag', u'Template:USS', u'Template:Use mdy dates', u'Template:WPMILHIST Infobox style', u'Module:Arguments', u'Module:Category handler', u'Module:Category handler/blacklist', u'Module:Category handler/config', u'Module:Category handler/data', u'Module:Category handler/shared', u'Module:Citation/CS1', u'Module:Citation/CS1/COinS', u'Module:Citation/CS1/Configuration', u'Module:Citation/CS1/Date validation', u'Module:Citation/CS1/Identifiers', u'Module:Citation/CS1/Utilities', u'Module:Citation/CS1/Whitelist', u'Module:Convert', u'Module:Convert/data', u'Module:Convert/text', u'Module:Data', u'Module:Hatnote', u'Module:Hatnote list', u'Module:InfoboxImage', u'Module:Namespace detect/config', u'Module:Namespace detect/data', u'Module:Navbar', u'Module:Navbox', u'Module:No globals', u'Module:Ns has subpages', u'Module:Other ships', u'Module:Other uses', u'Module:Side box', u'Module:TableTools', u'Module:Unsubst', u'Module:WPMILHIST Infobox style', u'Module:WPSHIPS utilities', u'Module:Yesno']
-    templates2 = [u'Template:About', u'Template:Annulenes', u'Template:Authority control', u'Template:Border-radius', u'Template:Box-shadow', u'Template:Cascite', u'Template:Category handler', u'Template:Chem', u'Template:Chem/atom', u'Template:Chem/link', u'Template:ChemID', u'Template:Chem molar mass', u'Template:Chem molar mass/format', u'Template:Chembox', u'Template:Chembox AllOtherNames', u'Template:Chembox AllOtherNames/format', u'Template:Chembox Appearance', u'Template:Chembox AutoignitionPt', u'Template:Chembox BoilingPt', u'Template:Chembox CASNo', u'Template:Chembox CASNo/format', u'Template:Chembox CASNo/parametercheck', u'Template:Chembox CalcTemperatures', u'Template:Chembox ChEBI', u'Template:Chembox ChEBI/format', u'Template:Chembox ChEMBL', u'Template:Chembox ChEMBL/format', u'Template:Chembox ChemSpiderID', u'Template:Chembox ChemSpiderID/format', u'Template:Chembox DeltaHc', u'Template:Chembox DeltaHf', u'Template:Chembox Density', u'Template:Chembox Dipole', u'Template:Chembox ECHA', u'Template:Chembox ECNumber', u'Template:Chembox EUClass', u'Template:Chembox Elements', u'Template:Chembox Elements/molecular formula', u'Template:Chembox Entropy', u'Template:Chembox ExploLimits', u'Template:Chembox FlashPt', u'Template:Chembox Footer', u'Template:Chembox GHSPictograms', u'Template:Chembox GHSSignalWord', u'Template:Chembox HPhrases', u'Template:Chembox Hazards', u'Template:Chembox HeatCapacity', u'Template:Chembox Identifiers', u'Template:Chembox InChI', u'Template:Chembox InChI/format', u'Template:Chembox Jmol', u'Template:Chembox Jmol/format/sandbox', u'Template:Chembox KEGG', u'Template:Chembox KEGG/format', u'Template:Chembox LambdaMax', u'Template:Chembox Lethal amounts (set)', u'Template:Chembox LogP', u'Template:Chembox MagSus', u'Template:Chembox MainHazards', u'Template:Chembox MeltingPt', u'Template:Chembox MolShape', u'Template:Chembox NFPA', u'Template:Chembox NIOSH (set)', u'Template:Chembox Odour', u'Template:Chembox OtherCpds', u'Template:Chembox PPhrases', u'Template:Chembox Properties', u'Template:Chembox PubChem', u'Template:Chembox PubChem/format', u'Template:Chembox RPhrases', u'Template:Chembox RTECS', u'Template:Chembox RefractIndex', u'Template:Chembox Related', u'Template:Chembox SDS', u'Template:Chembox SMILES', u'Template:Chembox SMILES/format', u'Template:Chembox SPhrases', u'Template:Chembox Solubility', u'Template:Chembox SolubilityInWater', u'Template:Chembox Structure', u'Template:Chembox Supplement', u'Template:Chembox Thermochemistry', u'Template:Chembox UNII', u'Template:Chembox UNII/format', u'Template:Chembox VaporPressure', u'Template:Chembox Viscosity', u'Template:Chembox headerbar', u'Template:Chembox image', u'Template:Chembox image cell', u'Template:Chembox image sbs', u'Template:Chembox image sbs cell', u'Template:Chembox removeInitialLinebreak', u'Template:Chembox subDatarow', u'Template:Chembox subHeader', u'Template:Chembox templatePar/formatPreviewMessage', u'Template:Chembox verification', u'Template:Chemboximage', u'Template:Chemspidercite', u'Template:Citation', u'Template:Citation needed', u'Template:Cite book', u'Template:Cite journal', u'Template:Cite web', u'Template:Collapsible list', u'Template:Column-width', u'Template:Commons', u'Template:Commons category', u'Template:Convert', u'Template:Cross', u'Template:Cycloalkenes', u'Template:DOI', u'Template:Delink', u'Template:Distinguish', u'Template:Doi', u'Template:Ebicite', u'Template:EditAtWikidata', u'Template:Eqm', u'Template:Fdacite', u'Template:Fix', u'Template:Fix/category', u'Template:Functional Groups', u'Template:Functional group', u'Template:GABAAR PAMs', u'Template:GHS02', u'Template:GHS07', u'Template:GHS08', u'Template:GHS exclamation mark', u'Template:GHS flame', u'Template:GHS health hazard', u'Template:GHS phrases format', u'Template:H-phrase text', u'Template:H-phrases', u'Template:Hazchem F', u'Template:Hazchem T', u'Template:Hide in print', u'Template:Hydrocarbons', u'Template:IDLH', u'Template:Icon', u'Template:Keggcite', u'Template:Key press', u'Template:Key press/core', u'Template:Keypress', u'Template:Linear-gradient', u'Template:Longitem', u'Template:Main', u'Template:Main article', u'Template:Main other', u'Template:Molecules detected in outer space', u'Template:NFPA 704 diamond', u'Template:NFPA 704 diamond/text', u'Template:Navbox', u'Template:Navbox generic', u'Template:Navbox subgroup', u'Template:Navbox subgroups', u'Template:Nist', u'Template:Nowrap', u'Template:Only in print', u'Template:P-phrase text', u'Template:P-phrases', u'Template:PGCH', u'Template:ParmPart', u'Template:Pp-move-vandalism', u'Template:PubChem', u'Template:R-phrase', u'Template:R11', u'Template:R16', u'Template:R36/38', u'Template:R45', u'Template:R46', u'Template:R48/23/24/25', u'Template:R65', u'Template:Reflist', u'Template:RubberBible86th', u'Template:S45', u'Template:S53', u'Template:Side box', u'Template:Sister project', u'Template:Stdinchicite', u'Template:Str left', u'Template:Su', u'Template:Tick', u'Template:Trim', u'Template:Wikiquote', u'Template:Wiktionary', u'Template:Yesno', u'Module:About', u'Module:Arguments', u'Module:Authority control', u'Module:Category handler', u'Module:Category handler/blacklist', u'Module:Category handler/config', u'Module:Category handler/data', u'Module:Category handler/shared', u'Module:Check for unknown parameters', u'Module:Citation/CS1', u'Module:Citation/CS1/COinS', u'Module:Citation/CS1/Configuration', u'Module:Citation/CS1/Date validation', u'Module:Citation/CS1/Identifiers', u'Module:Citation/CS1/Utilities', u'Module:Citation/CS1/Whitelist', u'Module:Collapsible list', u'Module:Convert', u'Module:Convert/data', u'Module:Convert/text', u'Module:Delink', u'Module:Distinguish', u'Module:EditAtWikidata', u'Module:Effective protection expiry', u'Module:Effective protection level', u'Module:File link', u'Module:Hatnote', u'Module:Hatnote list', u'Module:Icon', u'Module:Icon/data', u'Module:InfoboxImage', u'Module:Main', u'Module:Math', u'Module:Namespace detect/config', u'Module:Namespace detect/data', u'Module:Navbar', u'Module:Navbox', u'Module:No globals', u'Module:ParameterCount', u'Module:Protection banner', u'Module:Protection banner/config', u'Module:Side box', u'Module:String', u'Module:StringReplace', u'Module:Su', u'Module:TableTools', u'Module:TemplatePar', u'Module:Unsubst', u'Module:Wikidata', u'Module:Wikidata/i18n', u'Module:Yesno']
-    #print getInfoboxes(templates)
-    fill_article_table(num_art_want=1200)
+       fill_article_table(num_art_want=2400, multi=False)
